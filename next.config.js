1ec6dab51fb7f2185e153b5a7e7343f1cc9dfcb7
pizza3 <yugam.dhuriya@gmail.com>
2018-11-05 08:53:44 +0530

    changeg next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
     exportPathMap: async function (defaultPathMap) {
         return {
             '/': { page: '/' },
+            '/work': { page: '/work' },
+            '/contact': { page: '/contact' },
         }
     },
     assetPrefix: './'

c1ce759f9f1f8ac67f77c8325a4fa2dbb232a7d1
pizza3 <yugam.dhuriya@gmail.com>
2018-11-05 10:18:14 +0530

    changed asset prefix

--- next.config.js
+++ next.config.js
@@ -6,5 +6,5 @@ module.exports = {
             '/contact': { page: '/contact' },
         }
     },
-    assetPrefix: 'https://raw.githubusercontent.com/pizza3/pizza3.github.io/master/'
+    assetPrefix: './'
 }
